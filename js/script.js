e7a7c100f6ed5e125e3973e6062850b9d053e8fb
Douglas Amilcar Bonilla lemus <165966449+Doug-Bonilla@users.noreply.github.com>
2025-02-26 21:50:05 -0800

    Event handleing and if else

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var button = document.querySelector(".show-cat");
-//console.log(button.outerHTML)
 var cat = document.querySelector(".cat");
 //console.log(cat.outerHTML)
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    //console.log("yes");
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disapper");
+  } else {
+    //console.log("no");
+    cat.classList.add("show");
+    button.innerText = "Shoo, Cat!!";
+    button.classList.remove("disapper");
+  }
 });
